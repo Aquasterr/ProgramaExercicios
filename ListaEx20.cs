fd0c2bffabb2ac4c8c7e561dab67b7ec36de5ca8
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2022-09-05 18:42:41 -0300

    Create ListaEx20.cs

---
+++ ListaEx20.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Exercicio1
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            while (num >= 1)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+        }
+    }
+}
