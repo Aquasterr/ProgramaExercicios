b185f6f575773ea593aecca69b70b213608217c3
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2022-09-05 18:41:12 -0300

    Create ListaEx19.cs

---
+++ ListaEx19.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Exercicio1
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num <= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+    }
+}
