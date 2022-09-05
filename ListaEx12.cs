c3e42b6ef5959f0e992db965f956b0d68dbab82c
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2022-09-05 17:56:04 -0300

    Create ListaEx12.cs

---
+++ ListaEx12.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Exercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            string nome = (Console.ReadLine());
+
+            Console.WriteLine("Digite a Primeira Nota: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Segunda Nota: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Terceira Nota: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            double media = (n1 + n2 + n3) / 3;
+
+            Console.WriteLine("Nome do Aluno: {0}", nome);
+            Console.WriteLine("A Média do aluno é de: {0}", media);
+        }
+    }
+}
